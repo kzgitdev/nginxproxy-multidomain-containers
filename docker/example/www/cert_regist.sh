62ed82c5f4d443183032c2de2f21cd3f31a15c17
Kazuya.i <kzgitdev@outlook.com>
2021-12-08 00:49:08 +0900

    Create cert_regist.sh

---
+++ docker/example/www/cert_regist.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# cert registration script
+# 
+# USAGE:
+#   step1: docker-compose up -d web
+#   step2: exec cert_regist.sh to regist and get certifications
+EMAIL=user@example.com
+DOMAIN=subdomain.example.com
+
+docker-compose run --rm cert certonly \
+  --webroot -w /var/www/html \
+  -d ${DOMAIN} \
+  --non-interactive --agree-tos -m ${EMAIL} \
+  --dry-run -v
